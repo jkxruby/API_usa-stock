7529486ecb8ffece1c225992318a8b710cf1862d
jkxruby <jkxruby@gmail.com>
2017-09-13 15:36:59 +0800

    test

---
+++ README.md
@@ -0,0 +1,5 @@
+# 利用聚合数据的 API 查询股票
+
+![](http://okgqgpbx3.bkt.clouddn.com/blog/2017-04-01-Screen%20Shot%202017-04-01%20at%209.02.30%20AM.png)
+
+![](http://okgqgpbx3.bkt.clouddn.com/blog/2017-04-01-Screen%20Shot%202017-04-01%20at%209.02.40%20AM.png)
---
+++ db/migrate/20170331221103_create_usstocks.rb
@@ -0,0 +1,19 @@
+class CreateUsstocks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :usstocks do |t|
+      t.string :juhe_gid
+      t.string :name
+      t.string :lastestpri
+      t.string :openpri
+      t.string :formpri
+      t.string :limit
+      t.string :uppic
+      t.string :priearn
+      t.string :beta
+      t.date   :chtime
+      t.timestamps
+    end
+  end
+end
+
+ 
